216d966934090eeee61ead14266011b083250336
ashoaa <a.shoaahaghighi@gmail.com>
2023-06-27 18:23:05 +0330

    Create files

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Head from "next/head";
+
+const index = () => {
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          href="https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200X200.png"
+        />
+        <title>Next.js test</title>
+      </Head>
+      <h1>Test</h1>;
+    </>
+  );
+};
+
+export default index;
