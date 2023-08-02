2d9b04f95179a991b3b438c7c2df8f8ae1c9c706
ashoaa <a.shoaahaghighi@gmail.com>
2023-08-02 17:33:23 +0330

    Fix key warning

--- src/components/Item.js
+++ src/components/Item.js
@@ -38,81 +38,78 @@ const Item = ({ data }) => {
     <>
       <div className={classes["products"]}>
         {categories.map((category, index) => (
-          <>
-            <div key={index}>
-              <Typography
-                component="h2"
-                variant="h4"
-                sx={{
-                  marginBottom: "2rem",
-                  marginTop: "7rem",
-                  color: "#0c3e77",
-                }}>
-                {category[0].toUpperCase() + category.slice(1)}
-              </Typography>
+          <div key={index}>
+            <Typography
+              component="h2"
+              variant="h4"
+              sx={{
+                marginBottom: "2rem",
+                marginTop: "7rem",
+                color: "#0c3e77",
+              }}>
+              {category[0].toUpperCase() + category.slice(1)}
+            </Typography>
 
-              <Grid container spacing={5}>
-                {data
-                  .filter((item) => item.category === category)
-                  .map((product) => (
-                    <>
-                      <Grid
-                        item
-                        xs={6}
-                        md={4}
-                        lg={3}
+            <Grid container spacing={5}>
+              {data
+                .filter((item) => item.category === category)
+                .map((product) => (
+                  <Grid
+                    key={product.id}
+                    item
+                    xs={6}
+                    md={4}
+                    lg={3}
+                    sx={{
+                      marginBottom: "2rem",
+                    }}>
+                    <Paper elevation={5}>
+                      <div className={classes["item-image"]}>
+                        <img
+                          src={product.image}
+                          onClick={() => {
+                            router.push("/home/p" + product.id);
+                          }}
+                        />
+                      </div>
+                      <Typography
                         sx={{
-                          marginBottom: "2rem",
+                          fontSize: "13px",
+                          marginTop: "1rem",
+                          height: "50px",
+                          paddingLeft: "0.5rem",
+                          paddingRight: "0.5rem",
                         }}>
-                        <Paper elevation={5}>
-                          <div className={classes["item-image"]}>
-                            <img
-                              src={product.image}
-                              onClick={() => {
-                                router.push("/home/p" + product.id);
-                              }}
-                            />
-                          </div>
-                          <Typography
+                        {product.title.length < 60
+                          ? product.title
+                          : product.title.slice(0, 60) + "..."}
+                      </Typography>
+                      <div className={classes["item-pay"]}>
+                        <Typography
+                          sx={{
+                            paddingLeft: "0.5rem",
+                            fontWeight: "700",
+                          }}>
+                          ${product.price}
+                        </Typography>
+                        <div className={classes["item-buy"]}>
+                          <RemoveIcon
+                            onClick={removeItemHandler}
                             sx={{
-                              fontSize: "13px",
-                              marginTop: "1rem",
-                              height: "50px",
-                              paddingLeft: "0.5rem",
-                              paddingRight: "0.5rem",
-                            }}>
-                            {product.title.length < 60
-                              ? product.title
-                              : product.title.slice(0, 60) + "..."}
-                          </Typography>
-                          <div className={classes["item-pay"]}>
-                            <Typography
-                              sx={{
-                                paddingLeft: "0.5rem",
-                                fontWeight: "700",
-                              }}>
-                              ${product.price}
-                            </Typography>
-                            <div className={classes["item-buy"]}>
-                              <RemoveIcon
-                                onClick={removeItemHandler}
-                                sx={{
-                                  "&:hover": { cursor: "pointer" },
-                                }}></RemoveIcon>
-                              <AddIcon
-                                onClick={addItemHandler}
-                                sx={{
-                                  "&:hover": { cursor: "pointer" },
-                                }}></AddIcon>
-                            </div>
-                          </div>
-                        </Paper>
-                      </Grid>
-                    </>
-                  ))}
-              </Grid>
-            </div>
-          </>
+                              "&:hover": { cursor: "pointer" },
+                            }}></RemoveIcon>
+                          <AddIcon
+                            onClick={addItemHandler}
+                            sx={{
+                              "&:hover": { cursor: "pointer" },
+                            }}></AddIcon>
+                        </div>
+                      </div>
+                    </Paper>
+                  </Grid>
+                ))}
+            </Grid>
+          </div>
         ))}
       </div>
     </>
